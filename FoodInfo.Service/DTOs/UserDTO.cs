0fafe95613249c6c920b2fcf707869354e34baa9
DESKTOP-C72EGO5\fthcn <fthcnkrtrn@gmail.com>
2019-01-19 00:34:27 +0100

    isAdmin and isMod added to userDTO

--- FoodInfo.Service/DTOs/UserDTO.cs
+++ FoodInfo.Service/DTOs/UserDTO.cs
@@ -12,6 +12,8 @@ namespace FoodInfo.Service.DTOs
         public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public bool isAdmin { get; set; }
+        public bool isModerator { get; set; }
 
     }
 }
